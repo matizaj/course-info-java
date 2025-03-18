ad1276ef154f31c6435ddc7e8524c216754082cc
Mateusz <matzaj101@gmail.com>
2025-03-18 20:50:26 +0100

    add logger

--- src/main/java/com/ps/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/ps/courseinfo/cli/CourseRetriever.java
@@ -1,22 +1,25 @@
 package com.ps.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
     public static void main(String[] args) {
-        System.out.println("CourseRetriever Started!");
+        LOG.info("CourseRetriever Started!");
         if(args.length ==0) {
-            System.out.println("Please provide an author name as firts argument.");
+            LOG.warn("Please provide an author name as firts argument.");
             return;
         }
         try {
             retrieveCourses(args[0]);
         } catch(Exception e) {
-            System.out.println("Unexpected error: " + e.getMessage());
-            e.printStackTrace();
+            LOG.error("Unexpected error: " + e);
         }
 
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for author: " + authorId);
+        LOG.info("Retrieving courses for author: '{}'", authorId);
     }
 }
