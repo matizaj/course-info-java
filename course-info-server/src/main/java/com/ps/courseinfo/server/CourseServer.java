7efb0d2493084638808f4f562babc5de74a799c1
Mateusz <matzaj101@gmail.com>
2025-03-21 08:41:43 +0100

    add app configuration file

--- course-info-server/src/main/java/com/ps/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/ps/courseinfo/server/CourseServer.java
@@ -6,15 +6,36 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.Properties;
+import java.util.logging.LogManager;
 
 public class CourseServer {
+
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
+
+    private static String loadDatabaseFilename() {
+        try(var propStream = CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties props = new Properties();
+            props.load(propStream);
+            return props.getProperty("course-info.database");
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
+
     public static void main(String[] args) {
         LOG.info("Starting http server...");
-        CourseRepository repo = CourseRepository.openCourseRepository("./courses.db");
+        LOG.info("Loading db filename.." + loadDatabaseFilename());
+        CourseRepository repo = CourseRepository.openCourseRepository(loadDatabaseFilename());
         var config = new ResourceConfig().register(new CourseResource(repo));
 
         GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:8080"), config);
